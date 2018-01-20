0bca42edc831326055c32a2a0f01064d61646560
emilieez <emilie.m.zhang@gmail.com>
2018-01-19 19:25:21 -0800

    Remove code in building

--- building.py
+++ building.py
@@ -1,9 +1,5 @@
 class Building:
-    coords = {}
-
     def __init__(self, left, height, right):
         self.left = left
         self.height = height
         self.right = right
-
-        Building.coords[(left, right)] = height
